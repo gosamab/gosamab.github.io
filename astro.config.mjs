637d735b9de1d52dad89c391cc773c2265ac7add
Osama Azab <gosamab@hotmail.com>
2025-04-16 17:20:48 +0300

    Use tailwindcss cdn.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import mdx from "@astrojs/mdx";
 
 export default defineConfig({
 	output: "static",
 	base: "/",
-	integrations: [mdx()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
 });
