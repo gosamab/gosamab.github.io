f7973f5fd3645f972bcc74c6f182c7e8806bf022
Osama Azab <gosamab@hotmail.com>
2025-04-16 11:39:58 +0300

    Refined Home Page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
 	integrations: [mdx()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
