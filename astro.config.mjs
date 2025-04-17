a478d6789fcf3d3c5ebd566350f3b54a1f7ab418
Osama Azab <gosamab@hotmail.com>
2025-04-17 21:55:56 +0300

    refined issues.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	output: "static",
 	base: "/",
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
